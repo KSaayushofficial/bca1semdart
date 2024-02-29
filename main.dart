1e8b1997850287377cf3c6dbc832c15be6e1d5cb
KSaayushofficial <ksaayushofficial3@gmail.com>
2024-02-29 12:19:25 +0545

    Third commit

--- main.dart
+++ main.dart
@@ -1,13 +1,16 @@
+// DART PROGRAM FOR AVERAGE OF TWO NUMBERS.
+
 import 'dart:io';
  
-void main() {
-    print('Enter n1');
-    var n1 = double.parse(stdin.readLineSync()!);
-    print('Enter n2');
-    var n2 = double.parse(stdin.readLineSync()!);
-  
-    var output = n1 * n2;
-  
-    print('n1 * n2 = $output');
+void main(){
+    //read numbers from user
+    print('Enter x');
+    var x = double.parse(stdin.readLineSync()!);
+    print('Enter y');
+    var y = double.parse(stdin.readLineSync()!);
+ 
+    var sum = x + y;
+    var average = sum / 2;
+ 
+    print('average = $average');
 }
-// Dart program for multiplication
