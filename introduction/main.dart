cf1889d21e0eb2d606016be8789ea7419a739363
KSaayushofficial <ksaayushofficial3@gmail.com>
2024-03-03 09:42:58 +0545

    Homework1

--- introduction/main.dart
+++ introduction/main.dart
@@ -1,28 +1,4 @@
-import 'dart:io';
- 
-<<<<<<< HEAD
-void main() {
-    print('Enter n1');
-    var n1 = double.parse(stdin.readLineSync()!);
-    print('Enter n2');
-    var n2 = double.parse(stdin.readLineSync()!);
-  
-    var output = n1 * n2;
-  
-    print('n1 * n2 = $output');
-}
-// Dart program for multiplication
-=======
 void main(){
-    //read numbers from user
-    print('Enter x');
-    var x = double.parse(stdin.readLineSync()!);
-    print('Enter y');
-    var y = double.parse(stdin.readLineSync()!);
- 
-    var sum = x + y;
-    var average = sum / 2;
- 
-    print('average = $average');
+print ("\"mother\'s\"");
+print("\'mother\'s\'");
 }
->>>>>>> user4
